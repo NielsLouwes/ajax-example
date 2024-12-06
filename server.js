9b5047f7f228cdd509d9af52c418ec8e7d0334dc
Niels Louwes <nielslouwes45@gmail.com>
2024-12-06 12:27:11 +0100

    dont allow multiple requests of same data

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const response = "Initial content loaded.";
 const additionalResponse =
   "This is the additional content loaded via Ajax. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laborum debitis, recusandae ex voluptates blanditiis ipsam aliquam? Voluptas ex doloremque nisi cupiditate modi, illo quis asperiores laudantium esse molestias. Amet repellendus aspernatur libero nisi dolores unde, pariatur ex doloribus eveniet laboriosam saepe cumque distinctio quam, alias recusandae omnis atque aperiam commodi non possimus deleniti error excepturi accusantium? Explicabo delectus expedita illo deserunt dolores laudantium quibusdam corrupti mollitia, beatae porro sequi sint iste facere adipisci libero cupiditate doloribus obcaecati error praesentium assumenda repellendus, tenetur rerum! Provident deserunt expedita earum, dolore voluptatem iste sit asperiores autem beatae amet, mollitia et perferendis ea.";
 
+let loadedContent = false;
+
 // Endpoint to serve initial content
 app.get("/", (req, res) => {
   res.status(200).send(response);
@@ -23,8 +25,9 @@ app.get("/", (req, res) => {
 app.get("/more", (req, res) => {
   if (!additionalResponse) {
     res.status(404).send("No more content available");
-  } else {
+  } else if (loadedContent === false) {
     res.status(200).send(additionalResponse);
+    loadedContent = true;
   }
 });
 
